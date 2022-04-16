c6b132cf8d99dc1c40008d38c7743f509aa5d51a
Jakkrich Changgon <jakkrich.changgon@nstda.or.th>
2022-04-16 21:15:13 +0700

    Laravel 8 Tutorial - Form Validation

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,10 @@ class LoginController extends Controller
 
     public function loginSubmit(Request $request)
     {
+        $validateData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:12'
+        ]);
         $email = $request->input('email');
         $password = $request->input('password');
         return "Email: " . $email . " <br/>Password: " . $password;
